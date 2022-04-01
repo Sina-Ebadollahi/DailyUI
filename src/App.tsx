7266854f38a61cadf0ff8c95b5c0496cade729bd
Sina-Ebadollahi <Ebadollahisina@Gmail.com>
2022-04-01 19:02:57 +0430

    fifth commit | starting Daily3

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Daily1 from './Pages/Daily1/Daily1';
 import NotFound from './Pages/NotFound/NotFound';
 import IPUtility from './IPmanager/IPUtility';
 import Daily2 from './Pages/Daily2/Daily2';
+import Daily3 from './Pages/Daily3/Daily3';
 
 function App() {
   // get ip
@@ -17,7 +18,7 @@ function App() {
         <Routes>
           <Route path='/Daily1' element={<Daily1 />}/>
           <Route path='Daily2' element={<Daily2 />} />
-
+          <Route path='/Daily3' element={<Daily3 />}/>
 
           <Route path="*"  element={<NotFound />}/>
         </Routes>
---
+++ src/Pages/Daily3/Daily3.tsx
@@ -0,0 +1,118 @@
+import { Avatar } from "@mui/material";
+import { useState } from "react";
+import styled from "styled-components";
+// customs
+const primaryColor = '#132B50';
+type CustomDivType = {
+    d?:string | null;
+    jSelf?: string | null;
+    jContent?: string | null;
+    aItems?: string | null;
+    w?: string | null;
+    h?: string | null;
+    fDirection?: string | null;
+}
+const CustomDiv = styled.div`
+    display: ${(s: CustomDivType) => s.d};
+    justify-self: ${(s: CustomDivType) => s.jSelf};
+    justify-content: ${(s: CustomDivType) => s.jContent};
+    align-items: ${(s: CustomDivType) => s.aItems};
+    width: ${(s: CustomDivType) => s.w};
+    height: ${(s: CustomDivType) => s.h};
+`
+
+// components
+const Daily3Section = styled.section`
+    background-color: #919AAB;
+    min-width: 100vw;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+type DivType = {
+    mh: string | null;
+    mw: string | null;
+    bgColor: string | null;
+    bRadius?: number | null;
+    
+}
+const MainDiv = styled.div`
+    min-height: ${(s: DivType) => s.mh};
+    min-width: ${(s: DivType) => s.mw};
+    background-color: ${(s: DivType) => s.bgColor};
+    overflow: hidden;
+    border-radius: ${(s: DivType) => s.bRadius}px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+const MainWrapper = styled.div`
+    min-height: 55vh;
+    min-width: 80%;
+    border: solid black 2px;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+`
+const BoxContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: ${(s:CustomDivType) => s.fDirection};
+    width: ${(s: CustomDivType) => s.w};
+    justify-self: ${(s: CustomDivType) => s.jSelf};
+    justify-content: ${(s: CustomDivType) => s.jContent};
+`
+type MainHeaderType = {
+    fz?: number | null;
+
+}
+const MainHeader = styled.h1`
+    font-size: ${(s: MainHeaderType) => s.fz ? s.fz : 3.5}rem;
+    color: ${primaryColor};
+`
+export default function Daily3(): JSX.Element{
+    const [passwordData, setPasswordData] = useState<string>('');
+    return(
+        <Daily3Section>
+            <MainDiv bRadius={20} bgColor='#fff' mh={'70vh'} mw={'50vw'}>
+                <MainWrapper>
+                    <BoxContainer>
+                        <MainHeader>Enter your password</MainHeader>
+                    </BoxContainer>
+                    <BoxContainer >
+                        <BoxContainer w={'30vw'} jContent={'flex-start'}>
+                            <Avatar sx={{marginLeft: '2rem',width: '70px', height: '70px'}} src="https://avatars.githubusercontent.com/u/34670769?v=4" alt="Avatar" />
+                            <BoxContainer fDirection={'column'}>
+                                <MainHeader  fz={1}>Bussiness Account</MainHeader>
+                                <MainHeader fz={1.3}>Sarah Bills</MainHeader>
+                            </BoxContainer>
+                        </BoxContainer>
+                    </BoxContainer>
+                    <BoxContainer>
+                        <AuthenticationPart passwordData={passwordData} setPasswordData={setPasswordData} />
+                    </BoxContainer>
+                    <BoxContainer></BoxContainer>
+                </MainWrapper>
+            </MainDiv>
+        </Daily3Section>
+    )
+}
+const AuthContainer = styled.div`
+
+`
+type AuthenticationPartType = {
+    passwordData: string;
+    setPasswordData: React.Dispatch<React.SetStateAction<string>>;
+}
+const AuthenticationPart = (props: AuthenticationPartType): JSX.Element => {
+    
+    return(
+        <AuthContainer>
+
+        </AuthContainer>
+    )
+}
