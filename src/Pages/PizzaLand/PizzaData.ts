c69d63ff8fc8f2b11ad8407d7a13d47d1236c279
Sina-Ebadollahi <Ebadollahisina@Gmail.com>
2022-06-01 00:12:25 +0430

    sixteenth commit | dev

--- src/Pages/PizzaLand/PizzaData.ts
+++ src/Pages/PizzaLand/PizzaData.ts
@@ -1,3 +1,3 @@
 export const pizzaData = [
-    {img: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwallpaperaccess.com%2Fpizza&psig=AOvVaw1Zds7ml9p4vLbEpjnbDl-n&ust=1653492073089000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCNjtmo-4-PcCFQAAAAAdAAAAABAD',productName: "Pizza Peperroni",}
+    {img: 'https://wallpaperaccess.com/full/866645.jpg',productName: "Pizza Peperroni", productIngredients: ["Italian cheese", "tomato", "Flour", "Mozarella"]},
 ]
--- src/Pages/PizzaLand/PizzaLand.tsx
+++ src/Pages/PizzaLand/PizzaLand.tsx
@@ -22,18 +22,122 @@ export default function PizzaLand() {
           <IntroWrapper>
             <IntroFunction />
           </IntroWrapper>
+          <PizzaCard />
       </MainContainer>
   )
 }
 const BackgroundContainer = styled.div`
-  background-image: url(${(s: {imgSrc: string}) => s.imgSrc});
+height: 60vh;
+position: relative;
   background-repeat: no-repeat;
+`
+const BackgroundImage = styled.img`
+opacity: 0.7;
+width: 100%;
+height: 100%;
+  object-fit: cover;
+`
+const PizzaTextContainer = styled.div`
+  min-width: 20vw;
+  height: 30vh;
+  position: absolute;
+  left: 5%;
+  bottom: 30%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+const PizzaHeader = styled.h2`
+  font-size: 4.5rem;
+color: white;
+  text-align: center;
+  line-height: 3rem;
+  text-shadow: 2px 4px 2px black;
+`
+const PizzaParagraph = styled.p`
+margin-top: 2rem;
+  font-size:1.5rem;
+  color: black;
+  letter-spacing: 0.8rem;
+  text-shadow: 1px 2px 1px white;
+  text-align: center;
 `
 const IntroFunction = (): JSX.Element => {
 
   return(
-    <BackgroundContainer imgSrc='../../../assets/images/866645.jpg'>
-
+    <BackgroundContainer>
+      <BackgroundImage src={pizzaData[0].img} />
+      <PizzaTextContainer>
+        <PizzaHeader>
+          Greatest Pizza <br/> ever
+        </PizzaHeader>
+        <PizzaParagraph>
+          You Will enjoy it!
+        </PizzaParagraph>
+      </PizzaTextContainer>
     </BackgroundContainer>
   )
 }
+// pizza reserve card
+const PizzaCardContainer = styled.div`
+  min-height: 40vh;
+  min-width: 100vw;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+`
+const PizzaCardWrapper = styled.div`
+  min-height: 80%;
+  min-width: 20%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  &:hover{
+    padding: 8rem;
+  }
+`
+const PizzaCardImage = styled.img`
+  object-fit: fill;
+  max-width: 80%;
+  max-height: 40%;
+  
+
+`
+
+
+
+
+
+
+
+const PizzaCard = (): JSX.Element => {
+
+  return(
+    <PizzaCardContainer>
+      {pizzaData.map((e, i) => {
+        if(i > 0){
+          return(
+            <PizzaCardWrapper>
+              <PizzaCardImage src={e.img}  />
+            </PizzaCardWrapper>
+          )
+        }
+      })}
+    </PizzaCardContainer>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
