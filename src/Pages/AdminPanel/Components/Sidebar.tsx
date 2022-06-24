9fa3cd8ee5dc2e7308b84f573cc10867574438c5
Sina-Ebadollahi <Ebadollahisina@Gmail.com>
2022-06-24 08:55:48 +0430

    dev

--- src/Pages/AdminPanel/Components/Sidebar.tsx
+++ src/Pages/AdminPanel/Components/Sidebar.tsx
@@ -1,8 +1,11 @@
+import { SvgIconTypeMap } from "@mui/material";
+import { OverridableComponent } from "@mui/material/OverridableComponent";
 import { Col, Container, Image, Row } from "react-bootstrap";
 import styled from "styled-components";
-
+import { SidebarAssetsArr } from "./SidebarAssets";
 //types
 import { authInfoReducerType } from "../../../ReduxManager/ReduxMain";
+import { useNavigate } from "react-router-dom";
 //
 const SidebarContainer = styled.div`
   min-height: 100vh;
@@ -21,11 +24,30 @@ export default function Sidebar({
 }) {
   return (
     <div
-      style={{ minWidth: "10vw" }}
-      className="h-100 bg-dark min-vh-100 "
-    ></div>
+      style={{ minWidth: "10vw", maxWidth: "15vw" }}
+      className="h-100 bg-dark min-vh-100 d-flex flex-column"
+    >
+      {SidebarAssetsArr.map((e) => {
+        return <SidebarIconContainer info={e}></SidebarIconContainer>;
+      })}
+    </div>
   );
 }
-const SidebarIconContainer = (): JSX.Element => {
-  return <SidebarSeprator></SidebarSeprator>;
+type SidebarIconContainerParam = {
+  buttonText: string;
+  buttonIcon: OverridableComponent<SvgIconTypeMap<{}, "svg">>;
+  buttonNavLink: string;
+};
+const SidebarIconContainer = ({
+  info,
+}: {
+  info: SidebarIconContainerParam;
+}): JSX.Element => {
+  const nav = useNavigate();
+  return (
+    <Row className="d-flex flex-row" onClick={(e) => nav(info.buttonNavLink)}>
+      {<info.buttonIcon />}
+      {info.buttonText}
+    </Row>
+  );
 };
---
+++ src/Pages/AdminPanel/Components/SidebarAssets.ts
@@ -0,0 +1,11 @@
+import DashboardIcon from '@mui/icons-material/Dashboard';
+
+
+
+export const SidebarAssetsArr = [
+    {
+        buttonText: "Dashboard",
+        buttonIcon: DashboardIcon,
+        buttonNavLink: "/admin/Dashboard",
+    }
+]
